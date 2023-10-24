10fe1b7f375c5f057189f0ebb639f5e3b1487833
claudyadestine <144087128+claudyadestine@users.noreply.github.com>
2023-10-24 17:25:43 +0700

    Add files via upload

---
+++ LinearSearch08_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+   
+class LinearSearch08_1 {
+  public static void main(String args[]){  
+    Scanner sc = new Scanner(System.in);
+   
+    int [] nilaiKey = new int [10];
+        int jmlKey = 0;
+        int hasil = 0;
+        int cari = 0;
+        
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        jmlKey = sc.nextInt();
+
+        for (int i = 0; i < jmlKey; i++) {
+            System.out.print("Masukkan elemen array ke-"+(i+1)+" : ");
+            nilaiKey[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        cari = sc.nextInt();
+        for (int i = 0; i < jmlKey; i++){
+            if (nilaiKey[i] == cari){
+                hasil = i;
+                System.out.println("Key ada di posisi ke- " +i);
+                break;
+            }
+        }
+
+  }
+}
