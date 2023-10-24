cbed1e1fb22f464e529f49d2f0fe8b122da33c08
claudyadestine <144087128+claudyadestine@users.noreply.github.com>
2023-10-24 15:02:28 +0700

    Add files via upload

---
+++ ArrayBilangan08_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ArrayBilangan08_1{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+            int[] bil = {5, 13, -7, 17};
+        
+            for (int i : bil ){
+                System.out.println(i);
+            }
+    }
+}
